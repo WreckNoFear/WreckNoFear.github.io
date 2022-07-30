fae610c44a5804c65a2671331fe417c48216e71b
WreckNoFear <69378029+WreckNoFear@users.noreply.github.com>
2022-07-30 22:57:43 +1000

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const menu = document.querySelector(".menu-items");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+const navBtn = document.querySelector(".navbar-btn");
+
+menuBtn.addEventListener("click", function() {
+    menu.classList.add("active");
+    menuBtn.classList.add("hide");
+});
+cancelBtn.addEventListener("click", function() {
+    menu.classList.remove("active");
+    menuBtn.classList.remove("hide");
+});
+navBtn.addEventListener("click", function() {
+    menu.classList.remove("active");
+    menuBtn.classList.remove("hide");
+})
+
+window.addEventListener('scroll', function () {
+    let nav = document.querySelector('nav')
+    let windowPos = window.scrollY > 0;
+    nav.classList.toggle('nav-scroll', windowPos);
+});
